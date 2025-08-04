96ad9ba6017be901d6f4b4a6dda4db6f0248ad23
Mee-blip <drakedicky16@gmail.com>
2025-08-05 00:59:52 +0100

    Update settingss.js

--- settingss.js
+++ settingss.js
@@ -1,10 +1,10 @@
 
 module.exports = {
-  SESSION_ID: "",  // add your Session Id here
+  SESSION_ID: "malvin~AnETRAqL#0vDJCUdPOVhExi0dsb6LNTeoS3G1LaA1JV607-jXPwY",  // add your Session Id here
   
-  OWNER_NUMBER: "263714757857", // put your phone number here
+  OWNER_NUMBER: "2349078904723", // put your phone number here
   
   PREFIX: ".", // prefix (e.g., ., /, !, *)
   
-  TIMEZONE: "Africa/Harare" //put your country timeZone....leave blank if u don't know.
+  TIMEZONE: "West Africa standard time" //put your country timeZone....leave blank if u don't know.
 };
